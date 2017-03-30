f195b3df57d71ce39e08b01c8ba0d389361f2257
Robert Jewett <robertjewett@Roberts-MacBook-Pro.local>
2017-03-30 14:31:27 -0700

    pages linked

--- apps/compendium/views.py
+++ apps/compendium/views.py
@@ -4,4 +4,4 @@ from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, 'compendium/index.html')
+    return render(request, 'compendium/tester.html')
