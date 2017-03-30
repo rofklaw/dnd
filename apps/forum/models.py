da04111b26318d564cac856229041f8331e09cb5
ebergstein <ebergstein@sbcglobal.net>
2017-03-30 10:49:25 -0700

    Added forums.

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from ..logreg.models import User
 # Create your models here.
+
+class Post(models.Model):
+	text = models.CharField(max_length=255)
+	poster = models.ForeignKey(User, related_name = "posts")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+	
+class Comment(models.Model):
+	text = models.CharField(max_length=255)
+	poster = models.ForeignKey(User, related_name = "comments")
+	post = models.ForeignKey(Post, related_name = "comments")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
