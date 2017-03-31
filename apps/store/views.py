e566aa13dad241b3f1c360636ff87a560145d988
Briggs McKnight <briggs.mcknight@gmail.com>
2017-03-30 18:36:45 -0700

    modified css

--- apps/store/views.py
+++ apps/store/views.py
@@ -11,30 +11,6 @@ stripe.api_key = "sk_test_kqjvn71sExbxXooSaRrcSYrg"
 
 
 def index(request):
-<<<<<<< HEAD
-    pass
-    # products = Product.objects.all()
-    # for product in products:
-    #     print product.name
-    #     print product.picture
-    # data = {
-    # 'privilege': 0,
-    # 'merch': products
-    # }
-    # try:
-    #     # user = User.objects.get(id = request.session['id'])
-    #     admincheck = Admin.objects.get(users__id = request.session['id'])
-    #     data['privilege'] = admincheck.privilege_level
-    #     print "tried"
-    # except:
-    #     pass
-    # print data['privilege']
-    # print request.session['id']
-    #
-    # return render(request, 'store/index.html', data)
-
-=======
->>>>>>> 750e508706ff24652942588bca32b171c5787094
     cart_id = request.session['id']
     products = Product.objects.all()
     for product in products:
@@ -56,10 +32,7 @@ def index(request):
     print request.session['id']
 
     return render(request, 'store/index.html', data)
-<<<<<<< HEAD
-=======
 
->>>>>>> 750e508706ff24652942588bca32b171c5787094
 def curate(request):
     form = ProductForm()
     context = {
@@ -68,16 +41,7 @@ def curate(request):
     return render(request, 'store/curate.html', context)
 
 def create_merch(request):
-<<<<<<< HEAD
-    pass
-    # try:
-    #     admincheck = Admin.objects.get(users__id = request.session['id'])
-    # except:
-    #     return redirect(reverse('store:index'))
-    # Product.objects.create(name = request.POST['name'], description = request.POST['description'], picture = request.FILES['picture'], price = request.POST['price'], admin = admincheck)
-    # return redirect(reverse('store:index'))
-=======
->>>>>>> 750e508706ff24652942588bca32b171c5787094
+
     try:
         admincheck = Admin.objects.get(users__id = request.session['id'])
     except:
